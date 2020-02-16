73e77b91baad6443f9dd16755e47831e9b7f33d1
colorfulgreen <colorfulgreen@gmail.com>
2020-02-16 11:18:21 +0800

    linked-list

---
+++ linked-list/list.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "list.h"
+
+struct Node {
+    ElementType Element;
+    Position Next;
+};
+
+int IsEmpty(List L) {
+    return L->Next == NULL;
+}
+
+int IsLast(Position P, List L) {
+    return P->Next == NULL;
+}
+
+Position Find(ElementType X, List L) {
+    Position P;
+
+    P = L->Next;
+    while(P != NULL && P->Element != X)
+        P = P->Next;
+
+    return P;
+}
+
+void Delete(ElementType X, List L) {
+    Position P, TmpCell;
+    P = FindPrevious(X, L);
+    if(!IsLast(P, L)) {
+        TmpCell = P->Next;
+        P->Next = TmpCell->Next;
+        free(TmpCell);
+    }
+}
+
+Position FindPrevious(ElementType X, List L) {
+    Position P;
+
+    P = L;
+    while(P->Next != NULL && L->Next->Element != X)
+        P = P->Next;
+
+    return P; 
+}
+
+void Insert(ElementType X, List L, Position P) {
+    Position TmpCell;
+    
+    TmpCell = malloc(sizeof(struct Node));
+    if(TmpCell == NULL) {
+        perror("Out of space!!!");
+        exit(EXIT_FAILURE);
+    }
+
+    TmpCell->Element = X;
+    TmpCell->Next = P->Next;
+    P->Next = TmpCell;
+}
+
+void DeleteList(List L) {
+    Position P, Tmp;
+
+    P = L->Next;
+    L->Next = NULL;
+    while(P != NULL) {
+        Tmp = P->Next;
+        free(P);
+        P = Tmp;
+    }
+}
