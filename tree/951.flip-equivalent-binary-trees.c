e0a9c248cfb44c2ce24707ab89c18c8054f6dd1a
colorfulgreen <colorfulgreen@gmail.com>
2020-03-03 17:52:57 +0800

    tree 951.flip-equivalent-binary-trees

---
+++ tree/951.flip-equivalent-binary-trees.c
@@ -0,0 +1,23 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+
+bool flipEquiv(struct TreeNode* root1, struct TreeNode* root2) {
+    if(root1 == NULL && root2 == NULL)
+        return true;
+    if(root1 == NULL || root2 == NULL)
+        return false;
+    if(root1->val != root2->val)
+        return false;
+    if(!(flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right)))
+        return (flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left));
+    return true;
+}
+
+
